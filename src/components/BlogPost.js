d9d130846c54a554731ee4e01acad5be68b7ddb4
kurumi.sss <juni.walnut@gmail.com>
2023-11-08 15:23:51 +0900

    コミットメッセージ

---
+++ src/components/BlogPost.js
@@ -0,0 +1,5 @@
+const BlogPost = () => {
+  return <h1 class="bg-primary text-white display-4">blog記事</h1>;
+};
+
+export default BlogPost;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1 class="bg-secondary text-white display-4">ヘッダー</h1>;
+};
+
+export default Header;
