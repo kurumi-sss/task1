3db607cad6fb4ae2ef789860406d39cab1a681ad
kurumi.sss <juni.walnut@gmail.com>
2023-11-12 16:26:42 +0900

    1

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,32 +1,32 @@
 const posts = [
   {
     id: 1,
-    title: '記事タイトル１',
-    thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-11T09:00:00.000Z',
-    categories: ['React', 'TypeScript'],
+    title: "記事タイトル１",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-11T09:00:00.000Z",
+    categories: ["React", "TypeScript"],
     content: `
       本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
   },
   {
     id: 2,
-    title: '記事タイトル２',
-    thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-10T09:00:00.000Z',
-    categories: ['HTML', 'CSS'],
+    title: "記事タイトル２",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-10T09:00:00.000Z",
+    categories: ["HTML", "CSS"],
     content: `
       本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
   },
   {
     id: 3,
-    title: '記事タイトル３',
-    thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-09T09:00:00.000Z',
-    categories: ['JavaScript'],
+    title: "記事タイトル３",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-09T09:00:00.000Z",
+    categories: ["JavaScript"],
     content: `
       本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
   },
-]
+];
 
 const BlogPost = () => {
   return (
@@ -54,27 +54,21 @@ const BlogPost = () => {
               <div className="blog-post-info">
                 <p>日付</p>
                 <div>
-                          {post.categories.map((category) => {
-                            return (
-                              <div
-                                key={category}
-                              >
-                                {category}
-                              </div>
-                          )
-                          })}
-                        </div>
-                        </div>
+                  {post.categories.map((category) => {
+                    return <div key={category}>{category}</div>;
+                  })}
+                </div>
+              </div>
 
               <a href="../data/Posts.js">{post.title}</a>
               {/* <p>{post.content}</p> */}
               <div dangerouslySetInnerHTML={{ __html: post.content }} />
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
