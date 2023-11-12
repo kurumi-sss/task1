6699660b9415990b1f1a5fb2343bb2860ff6527b
kurumi.sss <juni.walnut@gmail.com>
2023-11-12 15:25:13 +0900

    タイトル本文編集

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,7 +1,45 @@
+const posts = [
+  {
+    id: 1,
+    title: "記事タイトル１",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-11T09:00:00.000Z",
+    categories: ["React", "TypeScript"],
+    content: `
+      本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+  {
+    id: 2,
+    title: "記事タイトル２",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-10T09:00:00.000Z",
+    categories: ["HTML", "CSS"],
+    content: `
+      本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+  {
+    id: 3,
+    title: "記事タイトル３",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-09T09:00:00.000Z",
+    categories: ["JavaScript"],
+    content: `
+      本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+];
+
 const BlogPost = () => {
   return (
     <div className="blog-post">
       <ul className="blog-post-list">
+        <li>
+          <div className="blog-post-info">
+            <p>日付</p>
+            <p>カテゴリ</p>
+          </div>
+          <a href="../data/Posts.js">{posts[0].title}</a>
+          <p>{posts[0].content}</p>
+        </li>
         <li>
           <div className="blog-post-info">
             <p>日付</p>
