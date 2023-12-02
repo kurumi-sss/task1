67ca5bad776a25475b6c42aa8aa517fd373d8ecd
kurumi.sss <juni.walnut@gmail.com>
2023-12-02 21:50:54 +0900

    完成？

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -2,32 +2,44 @@ import * as React from "react";
 import { posts } from "../data/Posts";
 import { useParams } from "react-router-dom";
 
-const pageId = useParams();
-
-const post = posts.find((post) => {
-  return post.id === pageId;
-});
-
 const BlogDetail = () => {
+  const pageId = parseInt(useParams().id, 10);
+
+  const post = posts.find((post) => {
+    return post.id === pageId;
+  });
+
+  if (!post) {
+    return <p>該当する記事が見つかりません。</p>;
+  }
+
   return (
     <div className="blog-detail">
-      <img src="https://placehold.jp/800x400.png" />
+      <img src={post.thumbnailUrl} alt="" />
       <div className="blog-detail-info">
         <div className="blog-post-info">
           <div className="blog-date">
-            {pageId}
-            {post.createdAt}
+            {new Date(post.createdAt).toLocaleString()}
             {/* 日付 */}
           </div>
-          <div className="category">
-            <p>カテゴリー</p>
+          <div className="blog-category">
+            {post.categories.map((category) => {
+              return (
+                <div className="category" key={category}>
+                  {category}
+                </div>
+              );
+            })}
+            {/* カテゴリー */}
           </div>
         </div>
         <div className="blog-title">
-          <p>記事タイトル</p>
+          <h1>{post.title}</h1>
+          {/* 記事タイトル */}
         </div>
         <div className="blog-text">
-          <p>本文です。本文です。本文です。本文です。本文です。</p>
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          {/* 本文です。 */}
         </div>
       </div>
     </div>
